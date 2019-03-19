d7bb91616621a2de87375b958ace6f025d72db48
Nico <nicolas.lb@globant.com>
2019-03-19 16:48:19 -0300

    Requested changes done

--- app/src/main/java/com/example/nicocalculator/MainActivity.kt
+++ app/src/main/java/com/example/nicocalculator/MainActivity.kt
@@ -6,24 +6,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val setInitValueOperations = "Operations"
-    private val setInitValueResults = "Results"
-    private val setValuePressed = "Pressed"
-    private val setValueEmpty = "Empty"
+
+
+    companion object {
+        const val OPERATIONS_DEFAULT_TEXT = "Operations"
+        const val RESULTS_DEFAULT_TEXT = "Results"
+        const val SET_VALUE_PRESSED = "Pressed"
+        const val SET_VALUE_EMPTY = "Empty"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        text_results.text = setInitValueResults
-        text_operations.text = setInitValueOperations
+        text_results.text = RESULTS_DEFAULT_TEXT
+        text_operations.text = OPERATIONS_DEFAULT_TEXT
 
         button_0.setOnClickListener {
-            text_results.text = setValuePressed
+            text_results.text = SET_VALUE_PRESSED
         }
 
         button_delete.setOnClickListener {
-            text_results.text = setValueEmpty
+            text_results.text = SET_VALUE_EMPTY
         }
 
     }
