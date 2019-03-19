96327d51b827c6124196a20eafbcfbac6edab53b
Nico <nicolas.lb@globant.com>
2019-03-19 14:18:04 -0300

    Edited layout

--- app/src/main/java/com/example/nicocalculator/MainActivity.kt
+++ app/src/main/java/com/example/nicocalculator/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.nicocalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text_results.text = "Result"
+        text_operations.text = "Operations"
+
+        button_0.setOnClickListener {
+            text_results.text = "pressed"
+        }
+
     }
 }
